76356bab69377054d7ed5847945fe99ecddcfc8e
Nguyễn Mai Thy <thy.nguyen_cloudy@hcmut.edu.vn>
2023-04-25 15:27:19 +0700

    LVTN-95: Add JWT Verify to Backend

---
+++ back-end/src/frameworks-and-drivers/auth-guard/README.md
@@ -0,0 +1,11 @@
+# frameworks-and-drivers-auth-guard
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `nx test frameworks-and-drivers-auth-guard` to execute the unit tests via [Jest](https://jestjs.io).
+
+## Running lint
+
+Run `nx lint frameworks-and-drivers-auth-guard` to execute the lint via [ESLint](https://eslint.org/).
--- back-end/src/frameworks-and-drivers/vove-managed/vove-managed-api/src/main.ts
+++ back-end/src/frameworks-and-drivers/vove-managed/vove-managed-api/src/main.ts
@@ -7,12 +7,14 @@ import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app/app.module';
+import {AuthGuard} from "@back-end/frameworks-and-drivers/auth-guard";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     origin: process.env["NX_HOME_URL"],
   });
+  app.useGlobalGuards(app.get(AuthGuard));
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
   const port = process.env.PORT || 3333;
