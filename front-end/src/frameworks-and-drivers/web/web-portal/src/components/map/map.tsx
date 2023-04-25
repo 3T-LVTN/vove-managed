a77fe7832617ef30d5851724686623872ba9093c
Nguyễn Mai Thy <thy.nguyen_cloudy@hcmut.edu.vn>
2023-04-25 17:55:57 +0700

    LVTN-104: Change load map from script to load from function

--- front-end/src/frameworks-and-drivers/web/web-portal/src/components/map/map.tsx
+++ front-end/src/frameworks-and-drivers/web/web-portal/src/components/map/map.tsx
@@ -1,7 +1,7 @@
 import React, {ReactElement, useEffect} from "react";
-import { GoogleMap, HeatmapLayerF, useGoogleMap, useJsApiLoader } from "@react-google-maps/api";
+import {GoogleMap, HeatmapLayerF, useGoogleMap, useJsApiLoader} from "@react-google-maps/api";
 import {ActionIcon} from "@mantine/core";
-import { IconArrowsMaximize } from "@tabler/icons-react";
+import {IconArrowsMaximize} from "@tabler/icons-react";
 import styles from './map.module.css'
 import {SearchHeatmapModalGlobalState} from "@front-end/frameworks-and-drivers/global-states/sreach-heatmap-modal";
 import {SearchHeatmapModalInteractor} from "@front-end/application/interactors/sreach-heatmap-modal";
@@ -14,48 +14,44 @@ const containerStyle = {
 };
 
 export const Map = () => {
-  // TODO: fix the bug maps is undefine (loading prob)
-  // const { isLoaded } = useJsApiLoader({
-  //   id: "google-map-script",
-  //   googleMapsApiKey: process.env["NX_GOOGLE_API_KEY"]!,
-  //   libraries: ['visualization']
-  // });
+  const {isLoaded} = useJsApiLoader({
+    id: "google-map-script",
+    googleMapsApiKey: process.env["NX_GOOGLE_API_KEY"]!,
+    libraries: ['visualization']
+  });
 
-  const centerPoint = { lat: 10.7644912, lng: 106.702996 };
+  const centerPoint = {lat: 10.7644912, lng: 106.702996};
 
   const searchHeatmapModalGlobalState = new SearchHeatmapModalGlobalState()
   const searchHeatmapModalUsecase = new SearchHeatmapModalInteractor(searchHeatmapModalGlobalState)
   const searchHeatmapModalController = new SearchHeatmapModalController(searchHeatmapModalUsecase)
 
-  const [map, setMap] = React.useState(null);
-
-  const onLoad = React.useCallback(function callback(map: any) {
-    setMap(map);
-  }, []);
-
-  const onUnmount = React.useCallback(function callback(map: any) {
-    setMap(null);
-  }, []);
-
-  const heatmapData = [
-    {location: new google.maps.LatLng(10.764, 106.702), weight: 15},
-    {location: new google.maps.LatLng(10.764, 106.703), weight: 15},
-    {location: new google.maps.LatLng(10.764, 106.701), weight: 15},
-    {location: new google.maps.LatLng(10.764, 106.705), weight: 15},
-    {location: new google.maps.LatLng(10.764, 106.704), weight: 15},
-  ]
-
-  return (
+  const renderMap = () => {
+    const heatmapData = [
+      {location: new google.maps.LatLng(10.764, 106.702), weight: 15},
+      {location: new google.maps.LatLng(10.764, 106.703), weight: 15},
+      {location: new google.maps.LatLng(10.764, 106.701), weight: 15},
+      {location: new google.maps.LatLng(10.764, 106.705), weight: 15},
+      {location: new google.maps.LatLng(10.764, 106.704), weight: 15},
+    ]
+    return (
       <GoogleMap
         mapContainerStyle={containerStyle}
         center={centerPoint}
         zoom={15}
-        onLoad={onLoad}
-        onUnmount={onUnmount}
         options={{streetViewControl: false, fullscreenControl: false}}
       >
         <HeatmapLayerF data={heatmapData}/>
-        <div className={styles.buttonLayer}><ActionIcon size="lg" variant="light" onClick={() => searchHeatmapModalController.setIsModalOpened(true)}><IconArrowsMaximize size="2.125rem"/></ActionIcon></div>
+        <div className={styles.buttonLayer}>
+          <ActionIcon size="lg" variant="light"
+                      onClick={() => searchHeatmapModalController.setIsModalOpened(true)}>
+            <IconArrowsMaximize
+              size="2.125rem"/>
+          </ActionIcon>
+        </div>
       </GoogleMap>
-  )
+    )
+  }
+
+  return isLoaded ? renderMap() : <></>
 };
