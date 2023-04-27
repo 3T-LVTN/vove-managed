106e82e651475a0b9d7b89ca9f4550d09dd540f9
thinh.lth <thinh.lth@teko.vn>
2023-04-27 13:26:21 +0700

    fix not found be

--- front-end/src/frameworks-and-drivers/web/web-portal/src/views/users/userlist.tsx
+++ front-end/src/frameworks-and-drivers/web/web-portal/src/views/users/userlist.tsx
@@ -19,7 +19,7 @@ import {AppUserUseCase} from "@front-end/application/usecases/app-user";
 import {AppUserInteractor} from "@front-end/application/interactors/app-user";
 import {AppUserController} from "@front-end/interface-adapters/controllers/app-user";
 import {Query, UserFilter} from "@front-end/shared/utils";
-import {PageTitle} from "../components/page-title/page-title";
+
 
 const AppUserList = () => {
   const appUserRepository: AppUserRepository = new AppUserApi();
@@ -194,7 +194,7 @@ const AppUserList = () => {
 
   return (
     <>
-      <PageTitle title={"Users Management"}></PageTitle>
+      {/* <PageTitle title={"Users Management"}></PageTitle> */}
       <MantineReactTable
         columns={columns}
         data={tableData}
