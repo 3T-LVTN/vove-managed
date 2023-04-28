a8eba7f8f178764764621570d5c7bbb4976a55ef
cloudythy <cloudythy@atware.asia>
2023-04-28 16:43:11 +0700

    LVTN-163: Change 404 page

--- front-end/src/frameworks-and-drivers/web/web-portal/src/views/page-not-found/page-not-found.tsx
+++ front-end/src/frameworks-and-drivers/web/web-portal/src/views/page-not-found/page-not-found.tsx
@@ -1,25 +1,78 @@
-import {Anchor, Button, Container, Image, Space, Stack, Text} from "@mantine/core"
-import { useNavigate } from "react-router-dom";
+import {
+  createStyles,
+  Image,
+  Container,
+  Title,
+  Text,
+  Button,
+  SimpleGrid,
+  rem,
+} from '@mantine/core';
+import {useNavigate} from "react-router-dom";
+
+const useStyles = createStyles((theme) => ({
+  root: {
+    paddingTop: rem(80),
+    paddingBottom: rem(80),
+  },
+
+  title: {
+    fontWeight: 900,
+    fontSize: rem(34),
+    marginBottom: theme.spacing.md,
+    [theme.fn.smallerThan('sm')]: {
+      fontSize: rem(32),
+    },
+  },
+
+  control: {
+    [theme.fn.smallerThan('sm')]: {
+      width: '100%',
+    },
+  },
+
+  mobileImage: {
+    [theme.fn.largerThan('sm')]: {
+      display: 'none',
+    },
+  },
+
+  desktopImage: {
+    [theme.fn.smallerThan('sm')]: {
+      display: 'none',
+    },
+  },
+}));
+
 export const PageNotFound = () => {
   const navigate = useNavigate();
   const goBack = () => {
     navigate(-1);
   }
+
+  const {classes} = useStyles();
+
   return (
-    <Container size="sm" my="5%">
-          <Stack  align="center" justify="center">
-            <Image mx="auto" src={"https://lvtn-s3-vove-web.s3.ap-southeast-1.amazonaws.com/PageNotFound.png"} alt="Page Not Found"/>
-            <Space h="xs" />
-            <Text>We can't seem to find the page you're looking for.</Text>
-            <Text>Try going back to the previous page or <Anchor>contact us</Anchor> for more information.</Text>
-            <Space h="xs" />
-            <Button size="lg" radius="xl" onClick={goBack}>Go Back</Button>
-            {/*TODO: can send email to dev when clicking on contact*/}
-          </Stack>
+    <Container className={classes.root} style={{display:"flex", alignItems: "center", height: "70vh"}}>
+      <SimpleGrid spacing={80} cols={2} breakpoints={[{maxWidth: 'sm', cols: 1, spacing: 40}]}>
+        <Image src={"https://lvtn-s3-vove-web.s3.ap-southeast-1.amazonaws.com/PageNotFound.png"} alt="Page Not Found"
+               className={classes.mobileImage}/>
+        <div>
+          <Title className={classes.title}>Something is not right...</Title>
+          <Text color="dimmed" size="lg">
+            Page you are trying to open does not exist. You may have mistyped the address, or the
+            page has been moved to another URL. If you think this is an error contact support.
+          </Text>
+          <Button variant="outline" size="md" mt="xl" className={classes.control} onClick={goBack}>
+            Get back to previous page
+          </Button>
+        </div>
+        <Image src={"https://lvtn-s3-vove-web.s3.ap-southeast-1.amazonaws.com/Vove.png"} alt="Page Not Found"
+               className={classes.desktopImage}/>
+      </SimpleGrid>
     </Container>
   )
 }
-
 export default PageNotFound;
 
 
