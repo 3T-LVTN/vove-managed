2e6f10f41a032df7d9ed2b7a0fa89d00f2740488
Nguyen Mai Thy <nguyenmaithy@Nguyens-MacBook-Pro.local>
2023-04-29 21:16:54 +0700

    LVTN-172: Add status badge for inquiry summary

--- front-end/src/frameworks-and-drivers/web/web-portal/src/components/inquiry-summary/inquiry-summary.tsx
+++ front-end/src/frameworks-and-drivers/web/web-portal/src/components/inquiry-summary/inquiry-summary.tsx
@@ -1,23 +1,19 @@
-import {Card, createStyles, Stack, Text} from "@mantine/core";
+import {Badge, Card, createStyles, Stack, Text} from "@mantine/core";
 import {useNavigate} from "react-router-dom";
 
 const useStyles = createStyles((theme) => ({
   card: {
     '&:hover': {
-      background: theme.colors.gray[0],
-      transform: 'scale(1.01)',
-      boxShadow: theme.shadows.md,
-      cursor: "pointer"
+      background: theme.colors.gray[0], transform: 'scale(1.01)', boxShadow: theme.shadows.md, cursor: "pointer"
     },
   },
 }));
 
 export const inquirySummary = () => {
   const navigator = useNavigate();
-  const { classes } = useStyles();
+  const {classes} = useStyles();
 
-  return (
-    <Stack w="100%" h="72%" justify="stretch" spacing={10}>
+  return (<Stack w="100%" h="72%" justify="stretch" spacing={10}>
       <Card
         withBorder
         radius="md"
@@ -25,10 +21,13 @@ export const inquirySummary = () => {
         className={classes.card}
         onClick={() => navigator(`/users`)}
       >
-        <Text fz="xs" tt="uppercase" fw={400} c="dimmed">
-          Nguyen Mai Thy - 03/05/2023 15:15
-        </Text>
-        <Text fz="lg" fw={500}>
+        <div style={{display:"flex", justifyContent: "space-between"}}>
+          <Text fz="xs" tt="uppercase" fw={400} c="dimmed">
+            Nguyen Mai Thy - 03/05/2023 15:15
+          </Text>
+          <Badge variant={"light"} size={"xs"}>Opening</Badge>
+        </div>
+        <Text fz="lg" fw={500} c={"dark.4"}>
           The predict results at my living area is incorrect
         </Text>
       </Card>
@@ -40,28 +39,31 @@ export const inquirySummary = () => {
         className={classes.card}
         onClick={() => navigator(`/users`)}
       >
-        <Text fz="xs" tt="uppercase" fw={400} c="dimmed">
-          Le Tran Hoang Thinh - 09/04/2023 16:32
-        </Text>
-        <Text fz="lg" fw={500}>
+        <div style={{display:"flex", justifyContent: "space-between"}}>
+          <Text fz="xs" tt="uppercase" fw={400} c="dimmed">
+            Le Tran Hoang Thinh - 03/05/2023 15:15
+          </Text>
+          <Badge variant={"light"} size={"xs"} color={"red"}>Closed</Badge>
+        </div>
+        <Text fz="lg" fw={500} c={"dark.4"}>
           I can't find my place on your map
         </Text>
       </Card>
 
-      <Card
-        withBorder
-        radius="md"
-        w="100%"
-        className={classes.card}
-        onClick={() => navigator(`/users`)}
-      >
-        <Text fz="xs" tt="uppercase" fw={400} c="dimmed">
-          Le Tran Hoang Thinh - 09/04/2023 16:32
-        </Text>
-        <Text fz="lg" fw={500}>
-          I can't find my place on your map
-        </Text>
-      </Card>
+      {/*<Card*/}
+      {/*  withBorder*/}
+      {/*  radius="md"*/}
+      {/*  w="100%"*/}
+      {/*  className={classes.card}*/}
+      {/*  onClick={() => navigator(`/users`)}*/}
+      {/*>*/}
+      {/*  <Text fz="xs" tt="uppercase" fw={400} c="dimmed">*/}
+      {/*    Le Tran Hoang Thinh - 09/04/2023 16:32*/}
+      {/*  </Text>*/}
+      {/*  <Text fz="lg" fw={500}>*/}
+      {/*    I can't find my place on your map*/}
+      {/*  </Text>*/}
+      {/*</Card>*/}
 
     </Stack>
 
