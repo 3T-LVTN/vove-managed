32c7023e8ff6bf86d624a1e06831795edd869c42
thinh.lth <thinh.lth@teko.vn>
2023-05-05 13:19:32 +0700

    add multi environment pipeline

--- front-end/build.sh
+++ front-end/build.sh
@@ -1,2 +1,31 @@
-DOCKER_BUILDKIT=1 docker build -t vove-managed-fe .
+#!/bin/bash
+
+# Parse options
+options=$(getopt -o e:f: --long env:,env-file: -- "$@")
+eval set -- $options
+
+# Loop over options
+while true; do
+  case "$1" in
+    -e|--env)
+      export ENV="$2"
+      shift 2
+      ;;
+    -f|--env-file)
+      export ENV_FILE="$2"
+      shift 2
+      ;;
+    --)
+      shift
+      break
+      ;;
+    *)
+      echo "Invalid option: $1"
+      exit 1
+      ;;
+  esac
+done
+echo $ENV
+echo $ENV_FILE
+DOCKER_BUILD_KIT=1 docker build -t vove_managed_fe_$ENV .
 docker-compose up -d
