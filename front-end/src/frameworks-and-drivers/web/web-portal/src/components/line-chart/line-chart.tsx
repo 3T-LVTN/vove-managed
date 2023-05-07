197f609242ae64658847f61bdc0d542c6c3e86de
Nguyễn Mai Thy <thy.nguyen_cloudy@hcmut.edu.vn>
2023-05-07 19:20:16 +0700

    LVTN-255: create bar chart and line chart components

---
+++ front-end/src/frameworks-and-drivers/web/web-portal/src/components/line-chart/line-chart.tsx
@@ -0,0 +1,59 @@
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js';
+import {Line} from 'react-chartjs-2';
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+export interface LineChartProps {
+  title: string;
+  labels: string[];
+  datasets: {
+    label: string;
+    data: number[];
+    borderColor: string;
+    backgroundColor: string;
+  }[];
+}
+
+const LineChart = (props: LineChartProps) => {
+  const data = {
+    labels: props.labels,
+    datasets: props.datasets,
+  };
+
+  const options =
+    {
+      responsive: true,
+      plugins: {
+        legend: {
+          position: 'top' as const,
+        },
+        title: {
+          display: true,
+          text: props.title,
+        },
+      },
+    };
+
+  return (
+    <Line data={data} options={options}/>
+  )
+}
+
+export default LineChart;
