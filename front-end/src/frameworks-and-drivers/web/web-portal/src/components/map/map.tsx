ab164f129d785f33041645b7377c631f2e25e9f3
Nguyễn Mai Thy <thy.nguyen_cloudy@hcmut.edu.vn>
2023-05-08 16:01:13 +0700

    test with mock data

--- front-end/src/frameworks-and-drivers/web/web-portal/src/components/map/map.tsx
+++ front-end/src/frameworks-and-drivers/web/web-portal/src/components/map/map.tsx
@@ -6,62 +6,108 @@ import styles from './map.module.css'
 import {SearchHeatmapModalGlobalState} from "@front-end/frameworks-and-drivers/global-states/sreach-heatmap-modal";
 import {SearchHeatmapModalInteractor} from "@front-end/application/interactors/sreach-heatmap-modal";
 import {SearchHeatmapModalController} from "@front-end/interface-adapters/controllers/sreach-heatmap-modal";
-import axios from "axios";
-import { json } from "stream/consumers";
-import { useLogger } from "@mantine/hooks";
+import {axios} from "@front-end/frameworks-and-drivers/app-sync/axios";
 
 export interface MapProps {
   fullScreenControl: boolean
 }
 
-const containerStyle = {  
+const containerStyle = {
   width: "100%",
   height: "100%",
 };
 type HeatMapPointData = {
   long: number
-  lat: number 
+  lat: number
   weight?: number
-  idx? :number
+  idx?: number
 }
 type HeatMapData = {
   availableLocations?: Array<HeatMapPointData>
   missingLocation?: Array<HeatMapPointData>
 }
 
-const initPointData: Map<number,HeatMapPointData> = new Map([
- [1, {lat: 10.764, long: 106.702}],
- [2,{ lat: 10.764, long:  106.703}],
- [3,{ lat: 10.764, long:  106.701}],
- [4,{ lat: 10.764, long:  106.705}],
- [5,{ lat: 10.764, long:  106.704}],
+const initPointData: Map<number, HeatMapPointData> = new Map([
+  [1, {lat: 10.764, long: 106.702}],
+  [2, {lat: 10.764, long: 106.703}],
+  [3, {lat: 10.764, long: 106.701}],
+  [4, {lat: 10.764, long: 106.705}],
+  [5, {lat: 10.764, long: 106.704}],
 ])
 
-export const VoveMap =  () => {
+const mockData: Array<HeatMapPointData> = [
+  {
+    lat: 10.764,
+    long: 106.702,
+    weight: 5
+  },
+  {
+    lat: 10.764,
+    long: 106.703,
+    weight: 10
+  },
+  {
+    lat: 10.764,
+    long: 106.701,
+    weight: 15
+  },
+  {
+    lat: 10.764,
+    long: 106.705,
+    weight: 15
+  },
+  {
+    lat: 10.764,
+    long: 106.704,
+    weight: 15
+  },
+]
+
+const libraries: ("visualization" | "places" | "drawing" | "geometry" | "localContext")[] = ["visualization", "places"];
+
+export const VoveMap = () => {
   const {isLoaded} = useJsApiLoader({
     id: "google-map-script",
     googleMapsApiKey: process.env["NX_GOOGLE_API_KEY"]!,
-    libraries: ['visualization', 'places']
+    libraries,
   });
 
-  const [stateData , setData] =  useState(initPointData)
-  
-  const centerPoint = {lat: 10.7644912, lng: 106.702996};
+  const [stateData, setData] = useState(initPointData)
+  const [mapData, setMapData] = useState<HeatMapData>({})
+  const [heatmapData, setHeatmapData] = useState<google.maps.visualization.WeightedLocation[]>([])
 
-  useMemo(() => {
-    setData(stateData);
-  }, [stateData])
+  const centerPoint = {lat: 10.7644912, lng: 106.702996};
 
   const searchHeatmapModalGlobalState = new SearchHeatmapModalGlobalState()
   const searchHeatmapModalUsecase = new SearchHeatmapModalInteractor(searchHeatmapModalGlobalState)
   const searchHeatmapModalController = new SearchHeatmapModalController(searchHeatmapModalUsecase)
 
-  const renderMap = () => {
-    const data: HeatMapData =  await axios.post("/prediction", stateData).then((resp)=>resp.data).catch((e) => console.log(e))
-    const heatmapData = data.availableLocations?.forEach((value)=> {
-      const val = initPointData.get(value.idx?value.idx:0)
-      return {location: new google.maps.LatLng((val?val:value).lat, (val?val:value).long), weight: value.weight}
+  const fetchHeatmapData = () => {
+    const data = mapData.availableLocations ?? mockData;
+    data.forEach((value) => {
+      const val = initPointData.get(value.idx ? value.idx : 0)
+      const weightedLocation: google.maps.visualization.WeightedLocation = {
+        location: new google.maps.LatLng((val ? val : value).lat, (val ? val : value).long),
+        weight: value.weight ?? 0
+      };
+      heatmapData.push(weightedLocation);
+      setHeatmapData([...heatmapData])
     })
+    console.log(heatmapData)
+  }
+
+  useEffect(() => {
+    // axios.post("/prediction", stateData)
+    //   .then((resp) => setMapData(resp.data))
+    //   .catch((e) => console.log(e))
+  }, [])
+
+  useMemo(() => {
+    if (!isLoaded) return;
+    fetchHeatmapData();
+  }, [isLoaded])
+
+  const renderMap = () => {
     return (
       <GoogleMap
         mapContainerStyle={containerStyle}
@@ -81,5 +127,5 @@ export const VoveMap =  () => {
     )
   }
 
-  return isLoaded ? renderMap():null
+  return isLoaded ? renderMap() : null
 };
